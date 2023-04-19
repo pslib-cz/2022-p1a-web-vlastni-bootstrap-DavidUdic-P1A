2fc24c8b5af4a462b300b694930c637215383061
DavidUdic-P1A <david.udic.022@g.pslib.cz>
2023-04-19 19:45:23 +0200

    ZatimMeToBavi

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("calorie-form");
+
+form.addEventListener("submit", function(event) {
+	event.preventDefault();
+	calculateCalories();
+});
+
+function calculateCalories() {
+	const height = parseFloat(document.getElementById("height").value);
+	const weight = parseFloat(document.getElementById("weight").value);
+	const age = parseInt(document.getElementById("age").value);
+	const activity = parseFloat(document.getElementById("activity").value);
+
+	const maintainWeight = Math.round((10 * weight + 6.25 * height - 5 * age + 5) * activity);
+	const loseWeight = maintainWeight - 500;
+	const gainWeight = maintainWeight + 500;
+
+	document.getElementById("maintain-weight").innerHTML = maintainWeight;
+	document.getElementById("lose-weight").innerHTML = loseWeight;
+	document.getElementById("gain-weight").innerHTML = gainWeight;
+}
